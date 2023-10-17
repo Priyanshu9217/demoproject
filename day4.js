1244feb0c336228ad85c24b0a8aa59d4370aa055
Priyanshu Soni <pssoni9217@gmail.com>
2023-10-17 16:57:54 +0530

    code1

---
+++ day4.js
@@ -0,0 +1,77 @@
+// //switch case
+// let day="pn";
+// switch (day){
+//     case "a":
+//     document.write("today is monday");
+//     brake;
+//     case "pn":
+//     document .write("today is tuesday ");
+//     brake;
+//     case "ram":
+//     document .write("today is wed");
+//     brake;
+//     default:
+//         document .write("enter the valid day");
+// }
+
+//loop//
+
+//while loop//
+// let a=10;
+
+// while (a>=1){
+//     document.write(a + "hello pninfosys<br>");
+// //++a//prefix//
+// //a++//postfix
+//      a--;
+    
+// }
+
+
+// dowhile//
+
+// let a=100;
+
+// do{
+//     document.write(a+"hello pninfosy <br>");
+//     a=a+1;
+// }while(a<=2)
+
+
+
+//for loop//
+
+// for(let a=1 ; a<=10;  a++)
+// {
+//     document.write(a);
+// }
+
+// function
+
+// function itm(){
+//     document.write("hello itm","<br>")
+// }
+
+
+// itm()
+// itm()
+// itm()
+// itm()
+// itm()
+// itm()
+// itm()
+// itm()
+// itm()
+// itm()
+// itm()
+// itm()
+// itm()
+// itm()
+
+//function with parameter//
+
+function Sum(a,b){
+    let c=a+b;
+    document.write("sum:",c)
+}
+Sum(700,800)
